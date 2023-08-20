9a296c0d70fbb8dc891a54c1baba51a2dfb89b10
Abderrahmane Abdelouafi <edunwant@LEETs-iMac.local>
2023-08-20 12:07:27 +0100

    Creating Include files

---
+++ Includes/minishell.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ababdelo <ababdelo@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/19 21:07:02 by ababdelo          #+#    #+#             */
+/*   Updated: 2023/08/19 22:09:28 by ababdelo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "libft.h"
+# include "parser.h"
+# include "utils.h"
+# include "error.h"
+# include "lexer.h"
+# include "color.h"
+# include "builtins.h"
+# include "executor.h"
+
+# endif
